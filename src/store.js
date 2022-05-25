6309553c1256fe918d9f2fafc7a1680560a0fe90
yezizheng <yezz@yusys.com.cn>
2022-05-26 00:21:20 +0800

    [new feature] state & getters

--- src/store.js
+++ src/store.js
@@ -4,7 +4,41 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community',
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false },
+    ],
+    events: [
+      { id: 1, text: '...', organizer: '...' },
+      { id: 2, text: '...', organizer: '...' },
+      { id: 3, text: '...', organizer: '...' },
+      { id: 4, text: '...', organizer: '...' },
+    ],
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    getEventById: (state) => (id) => {
+      return state.events.find((event) => event.id === id)
+    },
+    doneTodos: (state) => {
+      return state.todos.filter((todo) => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    },
+  },
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>{{ getEventById(2) }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapGetters, mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['getEventById']),
+    ...mapState(['categories', 'user']),
+  },
+}
+</script>
